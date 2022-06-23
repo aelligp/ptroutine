98ac430782c071a93df849c302e10e67174bfd67
Pascal Aellig <pascal.aellig@hotmail.com>
2022-06-23 10:59:15 +0200

    changed rho0 to rhom0 to have a single number

--- DoubleDiffusiveConvection.m
+++ DoubleDiffusiveConvection.m
@@ -11,7 +11,7 @@ outdir  = '../Cluster/200resolution/intermediate/Ta8/out/';
 path    = strcat(outdir,runID);
 addpath(path);
 parfile =  [path ,'/', runID, '_par.mat'];
-contfile=  [path ,'/', runID, '_cont.mat']; %set to desired .mat file number
+contfile=  [path ,'/', runID, '_51.mat']; %set to desired .mat file number
 if exist(parfile,'file'); load(parfile); end
 if exist(contfile,'file'); load(contfile,'U','W','P','Pt','f','x','m', ...
         'phi','chi','mu','H','C','V','T','c','v','cm','cx','vm','vf', ...
@@ -35,28 +35,34 @@ rhom = rhom0 .* (1 - aTm.*(T-perT-273.15) - gCm.*(cm-(perCx+perCm)/2));
 rhox = rhox0 .* (1 - aTx.*(T-perT-273.15) - gCx.*(cx-(perCx+perCm)/2));
 rhoo =  rhom0.*ones(size(T)); %starting density
 
-%calculate the temperature difference of layers
-Ttop = mean(T(ZZ>0.5 & ZZ<1.5));
-Tbot = mean(T(ZZ>1.5 & ZZ<9.5));
+%Calculate the temperature difference of layers
+Ttop = mean(T(ZZ>0.5 & ZZ<2));
+Tbot = mean(T(ZZ>2 & ZZ<9.5));
 dT = Tbot - Ttop;
 %Calculate the composition difference
-ctop = mean(c(ZZ>0.5 & ZZ<1.5));
-cbot = mean(c(ZZ>1.5 & ZZ<9.5));
+ctop = mean(c(ZZ>0.5 & ZZ<2));
+cbot = mean(c(ZZ>2 & ZZ<9.5));
 dC = cbot - ctop;
 %Calculate crystallinitiy difference
-xtop = mean(x(ZZ>0.5 & ZZ<1.5));
-xbot = mean(x(ZZ>1.5 & ZZ<9.5));
+xtop = mean(x(ZZ>0.5 & ZZ<2));
+xbot = mean(x(ZZ>2 & ZZ<9.5));
 dX = xbot - xtop; 
 %Calculate density diff. m and x
-drhomx0 = rhox-rhom; 
+drhomx0 = rhox0-rhom0; 
 %Density difference to analyse buoyancy
-drhoT = dT.*aTm.*rhoo; 
-drhoC = dC.*gCx.*rhoo;
-drhox = dX.*drhomx0; 
+drhoT = dT.*aTm.*rhom0 
+drhoC = dC.*gCx.*rhom0
+drhox = dX.*drhomx0
+
+
+drhoTT = [3.0, 2.8, 3.27]
+drhoCC = [-5.7, -8.18, -9.41]
+drhoXX = [2.4, 11.72, 13.42]
 
 % figure(1)
-% plot(X,drhoC)
+% scatter(drhoTT,drhoCC,drhoXX);
 % hold on
-% plot(X,drhoT)
-% figure(2)
-% scatter(X,Z,drhox)
+% scatter(X,drhox)
+% scatter(X,drhoT)
+% 
+% legend('X','drhoC','drhox','drhoT')
