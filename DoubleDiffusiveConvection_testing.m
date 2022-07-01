c9748e5a697226630d5aaf181278fc17299dfac2
Pascal Aellig <pascal.aellig@hotmail.com>
2022-07-01 12:48:11 +0200

    dC variation

--- DoubleDiffusiveConvection_testing.m
+++ DoubleDiffusiveConvection_testing.m
@@ -7,11 +7,14 @@
 
 %define environment and load files
 runID   = '2D_Ta8_interm_N200';
-outdir  = '../Cluster/out/';
+outdir  = '../Cluster/200resolution/intermediate/Ta8/out'
+%outdir  = '../Cluster/out/';
 path    = strcat(outdir,runID);
+
 addpath(path);
+
 parfile =  [path ,'/', runID, '_par.mat'];
-contfile=  [path ,'/', runID, '_127.mat']; %set to desired .mat file number
+contfile=  [path ,'/', runID, '_cont.mat']; %set to desired .mat file number
 if exist(parfile,'file'); load(parfile); end
 if exist(contfile,'file'); load(contfile,'U','W','P','Pt','f','x','m', ...
         'phi','chi','mu','H','C','V','T','c','v','cm','cx','vm','vf', ...
@@ -35,29 +38,41 @@ rhom = rhom0 .* (1 - aTm.*(T-perT-273.15) - gCm.*(cm-(perCx+perCm)/2));
 rhox = rhox0 .* (1 - aTx.*(T-perT-273.15) - gCx.*(cx-(perCx+perCm)/2));
 rhoo =  rhom0.*ones(size(T)); %starting density
 
-%% Theramal density difference - stimmt
+%% Theramal density difference - correct
 %Calculate the temperature difference of layers
 Ttop = mean(T(ZZ>0.5 & ZZ<5));
 Tbot = mean(T(ZZ>5 & ZZ<8.5));
 Tcp  = mean(T(ZZ>8.5 & ZZ<10)); %CP = Cumulate pile
 dT = Tbot - Ttop;
 drhoT = (-rhom0.*aTm + rhox0.*aTx).*dT
-%% Compositional density difference - something wrong
+%% Compositional density difference - should be correct
 
-%Calculate the composition difference
-% cmtop = mean(cm(ZZ>0.5 & ZZ<5));
-% cxtop = mean(cx(ZZ>0.5 & ZZ<5));
-% cmbot = mean(cm(ZZ>5 & ZZ<9.5));
-% cxbot = mean(cx(ZZ>5 & ZZ<9.5));
-% dcm = cmbot - cmtop;
-% dcx = cxbot - cxbot;
-% drhoC = -rhom.*gCm.*(dcm - (perCx+perCm)./2) + rhox.*gCx.*(dcx - (perCx+perCm)./2)
-%% Compositional density difference 
-ctop = mean(c(ZZ>0.5 & ZZ<5))
-cbot = mean(c(ZZ>5 & ZZ<8.5));
-ccp  = mean(c(ZZ>8.5 & ZZ<10)); %CP = Cumulate pile
-dC = cbot - ctop;
-drhoC = -dC.*gCm.*rhom0
+% Calculate the composition difference
+% volume fraction
+cmtop = mean(cm(ZZ>0.5 & ZZ<5));
+cxtop = mean(cx(ZZ>0.5 & ZZ<5));
+cmbot = mean(cm(ZZ>5 & ZZ<9));
+cxbot = mean(cx(ZZ>5 & ZZ<9));
+% Density 
+rhomtop = mean(rhom(ZZ>0.5 & ZZ<5));
+rhombot = mean(rhom(ZZ>5 & ZZ<9));
+rhoxtop = mean(rhox(ZZ>0.5 & ZZ<5));
+rhoxbot = mean(rhox(ZZ>5 & ZZ<9));
+
+dcm = cmbot - cmtop;
+dcx = cxbot - cxbot;
+drhom = rhombot - rhomtop;
+drhox = rhoxbot - rhoxtop;
+drhoC = -dcm.*gCm.*drhom + dcx.*gCx.*drhox %compositional difference
+
+
+%drhoC = -rhom0.*gCm.*(dcm - (perCx+perCm)./2) + rhox0.*gCx.*(dcx - (perCx+perCm)./2)
+% %% Compositional density difference 
+% ctop = mean(c(ZZ>0.5 & ZZ<5));
+% cbot = mean(c(ZZ>5 & ZZ<8.5));
+% ccp  = mean(c(ZZ>8.5 & ZZ<10)); %CP = Cumulate pile
+% dC = cbot - ctop;
+% %drhoC = -dC.*gCm.*rhom0
 
 %% Crystalline density difference
 %Calculate crystallinitiy difference
@@ -65,6 +80,11 @@ xtop = mean(x(ZZ>0.5 & ZZ<5));
 xbot = mean(x(ZZ>5 & ZZ<9));
 xcp  = mean(x(ZZ>8.5 & ZZ<10)); %CP = Cumulate pile
 dX = xbot - xtop; 
+
+%Calculate melt fraction top and bot layer
+mtop = mean(m(ZZ>0.5 & ZZ<5));
+mbot = mean(m(ZZ>5 & ZZ<9));
+dm = mbot - mtop;
 
 %Calculate density diff. m and x
 drhoxm0 = rhox0-rhom0; % not sure if correct yet
@@ -77,20 +97,16 @@ drhomixtop = dX*(drhotop)+mean(rhom(ZZ>0.5 & ZZ<8));
 drhomixbot = dX*(drhobot)+mean(rhom(ZZ>8 & ZZ<9));
 
 %Density difference to analyse buoyancy
-drhoT = -dT.*aTm.*rhom0 
+% drhoT = -dT.*aTm.*rhom0 
 
 drhoX = dX.*drhoxm0
-drhoX = xbot*drhobot - xtop*drhotop
+%drhoX = xbot*drhobot - xtop*drhotop;
 
-drhotop = mean(rho(ZZ>0.5 & ZZ<7));
-drhobot = mean(rho(ZZ>7 & ZZ<9));
-
-drhomix = drhobot - drhotop
 sumrho = drhoC + drhoX + drhoT
 
-if sumrho <0 sprintf('unstable')
-else sprintf('stable')
-end
+% if sumrho <0 sprintf('unstable')
+% else sprintf('stable')
+% end
 %% rho bar calc of YQW
 
 rhoxbot = mean(rhox(ZZ>5 & ZZ<9)); 
@@ -99,19 +115,10 @@ rhombot = mean(rhom(ZZ>5 & ZZ<9));
 rhomtop = mean(rhom(ZZ>0.5 & ZZ<5));
 
 % rho_bar_bot - rho_bar_top
-drhobar = 0
+drhobar = (rhoxbot - rhoxtop).*xbot + (rhombot -rhomtop).*mbot + ...
+    (rhoxtop-rhomtop).*(xbot-xtop)
 
+drhobarbot = 1./((mbot./rhombot)+(xbot./rhoxbot)); % mixing density bottom layer
+drhobartop = 1./((mtop./rhombot)+(xtop./rhoxbot)); % mixiing density top layer
+drhobarmix = drhobarbot - drhobartop
 
-
-
-% drhoTT = [3.0, 2.8, 3.27]
-% drhoCC = [-5.7, -8.18, -9.41]
-% drhoXX = [2.4, 11.72, 13.42]
-
-% figure(1)
-% scatter(drhoTT,drhoCC,drhoXX);
-% hold on
-% scatter(X,drhox)
-% scatter(X,drhoT)
-% 
-% legend('X','drhoC','drhox','drhoT')
