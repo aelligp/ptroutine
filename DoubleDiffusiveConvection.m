ad9232d3ce36910f118a9d31923e07690a668e6b
Pascal Aellig <pascal.aellig@hotmail.com>
2022-07-01 15:33:26 +0200

    Major: correct equations, code is working

--- DoubleDiffusiveConvection.m
+++ DoubleDiffusiveConvection.m
@@ -1,19 +1,20 @@
-%% Double Diffusive Convection Analyses
+%% Double Diffusive COnvectoin Analyses
 % Use this script to analyse the different factors which influence the
-% layered convection in the nakhla model
+% layered convection in the nakhla model 
 % different density of T, c and x
 % Also applicable to differences in vertical convection speed of different
 % parameter sets
 
 %define environment and load files
 runID   = '2D_Ta8_interm_N200';
-outdir  = '../Cluster/200resolution/intermediate/Ta8/out/';
-
+% outdir  = '../Cluster/200resolution/intermediate/Ta8/out'
+outdir  = '../Cluster/out/';
 path    = strcat(outdir,runID);
+
 addpath(path);
 
 parfile =  [path ,'/', runID, '_par.mat'];
-contfile=  [path ,'/', runID, '_51.mat']; %set to desired .mat file number
+contfile=  [path ,'/', runID, '_127.mat']; %set to desired .mat file number
 if exist(parfile,'file'); load(parfile); end
 if exist(contfile,'file'); load(contfile,'U','W','P','Pt','f','x','m', ...
         'phi','chi','mu','H','C','V','T','c','v','cm','cx','vm','vf', ...
@@ -35,36 +36,70 @@ Zfc       = (Z(1:end-1)+Z(2:end))./2;
 [XXw,ZZw] = meshgrid(X,Zfc);
 rhom = rhom0 .* (1 - aTm.*(T-perT-273.15) - gCm.*(cm-(perCx+perCm)/2));
 rhox = rhox0 .* (1 - aTx.*(T-perT-273.15) - gCx.*(cx-(perCx+perCm)/2));
-rhoo =  rhom0.*ones(size(T)); %starting density
 
-%Calculate the temperature difference of layers
-Ttop = mean(T(ZZ>0.5 & ZZ<2));
-Tbot = mean(T(ZZ>2 & ZZ<9.5));
-dT = Tbot - Ttop;
-%Calculate the composition difference
-ctop = mean(c(ZZ>0.5 & ZZ<2));
-cbot = mean(c(ZZ>2 & ZZ<9.5));
-dC = cbot - ctop;
+%% Layer calculations
+% Calculate temperature differences of layers
+Ttop = mean(T(ZZ>0.5 & ZZ<5));
+Tbot = mean(T(ZZ>5 & ZZ<8.5));
+Tcp  = mean(T(ZZ>8.5 & ZZ<10)); % Cumulate pile
+
+% Calculate the composition difference
+% volume fraction
+cmtop = mean(cm(ZZ>0.5 & ZZ<5));
+cmbot = mean(cm(ZZ>5 & ZZ<9));
+cxtop = mean(cx(ZZ>0.5 & ZZ<5));
+cxbot = mean(cx(ZZ>5 & ZZ<9));
+
+%Calculate melt fraction top and bot layer
+mtop = mean(m(ZZ>0.5 & ZZ<5));
+mbot = mean(m(ZZ>5 & ZZ<9));
+
 %Calculate crystallinitiy difference
-xtop = mean(x(ZZ>0.5 & ZZ<2));
-xbot = mean(x(ZZ>2 & ZZ<9.5));
-dX = xbot - xtop; 
-%Calculate density diff. m and x
-drhomx0 = rhox0-rhom0; 
-%Density difference to analyse buoyancy
-drhoT = dT.*aTm.*rhom0 
-drhoC = dC.*gCx.*rhom0
-drhox = dX.*drhomx0
+xtop = mean(x(ZZ>0.5 & ZZ<5));
+xbot = mean(x(ZZ>5 & ZZ<9));
+xcp  = mean(x(ZZ>8.5 & ZZ<10)); %CP = Cumulate pile
+
+% xtal and melt densities of layers
+rhoxbot = mean(rhox(ZZ>5 & ZZ<9)); 
+rhoxtop = mean(rhox(ZZ>0.5 & ZZ<5));
+rhombot = mean(rhom(ZZ>5 & ZZ<9)); 
+rhomtop = mean(rhom(ZZ>0.5 & ZZ<5));
+
+% volume fractions of layers 
+mubot = mean(mu(ZZ>5 & ZZ<9)); 
+mutop = mean(mu(ZZ>0.5 & ZZ<5));
+chibot = mean(chi(ZZ>5 & ZZ<9)); 
+chitop = mean(chi(ZZ>0.5 & ZZ<5));
+
+% Calculated mixture density of model layers
+rhotop = mean (rho(ZZ>0.5 & ZZ<5));
+rhobot = mean (rho(ZZ>5 & ZZ<9));
+
+%% Density difference analyses
+
+drhoT = (chibot.*(-rhox0.*aTx.*(Tbot-Ttop)))+(mubot.*(-rhom0.*aTm.*(Tbot-Ttop)))
 
 
-drhoTT = [3.0, 2.8, 3.27]
-drhoCC = [-5.7, -8.18, -9.41]
-drhoXX = [2.4, 11.72, 13.42]
+drhoC = (chibot.*(-rhox0.*gCx.*(cxbot-cxtop)))+(mubot.*(-rhom0.*gCm.*(cmbot-cmtop))) %compositional difference
 
-% figure(1)
-% scatter(drhoTT,drhoCC,drhoXX);
-% hold on
-% scatter(X,drhox)
-% scatter(X,drhoT)
+
+drhoX = (rhoxtop -rhomtop).*(chibot-chitop)
+
+% mixture density difference rhobar = effect on temp + compo + xtals
+drhobar = chibot.*(-rhox0.*aTx.*(Tbot-Ttop))+(mubot.*(-rhom0.*aTm.*(Tbot-Ttop))) + ...
+(chibot.*(-rhox0.*gCx.*(cxbot-cxtop)))+(mubot.*(-rhom0.*gCm.*(cmbot-cmtop))) + ...
+(rhoxtop -rhomtop).*(chibot-chitop)
+
+
+rhobar = rhobot -rhotop % model mixture density
+
+
+if sumrho <0 sprintf('unstable')
+else sprintf('stable')
+end
+
+
+
+% xtal = (rhoxtop -rhomtop).*(chibot-chitop)
 % 
-% legend('X','drhoC','drhox','drhoT')
+% xtaleffect = rhoxtop.*chibot + rhomtop.*mubot - rhoxtop.*chitop - rhomtop.*mutop
